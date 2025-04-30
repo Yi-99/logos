e6efa1bfed43b73bdab67f7f0966bfd839b75ef5
ylim1699 <ylim.0348@gmail.com>
2025-04-30 01:01:32 -0600

    feat: added a sidebar button

---
+++ frontend/src/SideBar.tsx
@@ -0,0 +1,8 @@
+const SideBar = () => {
+    
+    return (
+       <button className="cursor-pointer transition-all hover:scale-110 hover:!border-black"><img src="/Screenshot 2025-04-30 005530.png" alt="side-bar image" className="w-5 h-4"/></button>
+    )    
+}
+
+export default SideBar;
