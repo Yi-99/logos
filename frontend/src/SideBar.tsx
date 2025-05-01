c8c5e1a132c26ac8637f0f8bc58a25e76466b6b7
ylim1699 <ylim.0348@gmail.com>
2025-04-30 21:48:15 -0600

    feat: sidebar slide feature

--- frontend/src/SideBar.tsx
+++ frontend/src/SideBar.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
@@ -5,37 +6,45 @@ import HistoryIcon from '@mui/icons-material/History';
 import SettingsIcon from '@mui/icons-material/Settings';
 
 const SideBar = () => {
+  const [isOpen, setisOpen] = useState(false);
+
+  const ToggleSideBar = () => {
+    setisOpen(!isOpen);
+  }
 
   return (
     <>
-        <aside className="w-64 bg-neutral-100 overflow-y-auto p-4 h-screen">
-          <button className="cursor-pointer transition-all hover:scale-110 hover:!border-black mb-2">
-            <MenuIcon />
+      <aside className="w-12 bg-neutral-100 p-4 h-screen overflow-y-auto">
+        <button onClick={ToggleSideBar} className="cursor-pointer transition-all hover:scale-110 mb-2 ">
+          <MenuIcon />
+        </button>
+      </aside>
+
+      <aside className={`transition-all duration-300 ${isOpen ? 'w-64' : 'w-0'} bg-neutral-100 h-screen overflow-y-auto`}>
+        <div className="flex gap-4 justify-end items-center mt-4 mr-4">
+          <button className="cursor-pointer transition-all hover:scale-110">
+            <SearchIcon />
           </button>
-            <div className="flex gap-4 justify-center items-center">
-              <button className="cursor-pointer transition-all hover:scale-110">
-                <SearchIcon />
-              </button>
-              <button className="cursor-pointer transition-all hover:scale-110">
-                <OpenInNewIcon />
-              </button>
-            </div>
-            <h3 className="font-bold mt-6 flex justify-start">Recent</h3>
-            <div className="">
-              
-            </div>
-            <div className="flex flex-col items-start font-bold gap-2 mt-6">
-              <button className="flex gap-2 cursor-pointer transition-all hover:scale-110">
-                <HistoryIcon />
-                Activity
-              </button>
-              <button className="flex gap-2 ursor-pointer transition-all hover:scale-110">
-                <SettingsIcon />
-                Settings
-              </button>
-            </div>
-            
-        </aside>
+          <button className="cursor-pointer transition-all hover:scale-110">
+            <OpenInNewIcon />
+          </button>
+        </div>
+        
+        <h3 className="font-bold mt-6 flex justify-start">Recent</h3>
+        <div className="">
+        </div>
+        
+        <div className="flex flex-col items-start font-bold gap-2 mt-6">
+          <button className="flex gap-2 cursor-pointer transition-all hover:scale-110">
+            <HistoryIcon />
+            Activity
+          </button>
+          <button className="flex gap-2 cursor-pointer transition-all hover:scale-110">
+            <SettingsIcon />
+            Settings
+          </button>
+        </div>   
+      </aside>
     </>
   )   
 }
